679e83ca1142a226e3b3f4a65268937c3ec03ca5
Jeffrey Kranenburg <jeff@Jeffreys-MacBook-Air.local>
2019-10-06 16:35:34 +1300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,67 @@
+import React, { Component, useEffect } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import Purchases from 'react-native-purchases';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  
+  state = { 
+    temp: "hello",
+    data: null,
+    entitlements: null
+  };
+
+  test = async () => {
+  
+    try {
+      //const entitlements = await Purchases.getEntitlements();
+      //showUpsellScreen(entitlements);
+
+      //this.setState({data : showUpsellScreen(entitlements), entitlements : entitlements})
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  componentDidMount() {
+    //Purchases.setDebugLogsEnabled(true);
+    //Purchases.setup("pBehIKhTiMBTqykcKmzkTeBnnXLWaUET", "unlock");
+    this.test()
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{(this.state.temp === null)}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ ios/Pods/Headers/Private/Purchases/RCEntitlement+Protected.h
@@ -0,0 +1,1 @@
+../../../Purchases/Purchases/RCEntitlement+Protected.h
